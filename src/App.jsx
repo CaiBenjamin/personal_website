3e26939ab1db78f3fdbe3f18dd4becb403939395
spelmage <spelscarmage@gmail.com>
2025-04-13 11:07:04 -0400

    updated ccs

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,23 @@ function App() {
   return (
     <Router>
       <div id="root">
-        <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px 20px' }}>
+        <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px 20px', fontFamily: "Chelsea Market" }}>
           <img src="/src/assets/transparent_corgi.png" alt="Nap Time" style={{ width: '75px', height: '75px', objectFit: 'cover', position: 'absolute', left: '20px', top: '10px' }} />
-          <nav style={{ display: 'flex', alignItems: 'center', position: 'absolute', right: '20px', top: '10px' }}>
+          <nav style={{ display: 'flex', alignItems: 'center', position: 'absolute', right: '20px', top: '10px', fontFamily: "Chelsea Market" }}>
             <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
             <Link to="/projects" style={{ margin: '0 10px' }}>Projects</Link>
             <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
             <button
               onClick={() => window.open('https://www.linkedin.com/in/benjamin-cai-a59822159/', '_blank')}
-              style={{ margin: '0 10px', padding: '5px 10px', cursor: 'pointer' }}
+              style={{ margin: '0 10px', padding: '5px 10px', cursor: 'pointer', fontFamily: "Chelsea Market" }}
             >
               Connect
             </button>
           </nav>
         </header>
         <Routes>
-          <Route path="/" element={<div style={{ textAlign: 'center', marginTop: '20px', fontFamily: 'Sour Gummy, cursive', color: '#F4A261', fontSize: '1.5em', padding: '20px 0' }}>
-            <p>Benjamin Cai is a software developer who thrives on building innovative projects, solving complex problems, and exploring new technologies.</p>
+          <Route path="/" element={<div style={{ textAlign: 'center', marginTop: '100px', fontFamily: "Chelsea Market", color: '#F4A261', fontSize: '1.5em', padding: '20px 0' }}>
+            <p style={{ maxWidth: '900px', margin: '1000 auto' }}>Benjamin Cai is a software developer who thrives on building innovative projects, solving complex problems, and exploring new technologies.</p>
           </div>} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
